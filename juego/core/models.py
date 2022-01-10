7a38c11102578759efe438db515303968f31a177
danielgalli <daniel.galli.lillo@gmail.com>
2022-01-10 04:09:38 -0300

    base

---
+++ juego/core/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Personaje(models.Model):
+    '''Carreras de cada sede'''
+    nombre = models.CharField(max_length=300)
+    nivel = models.IntegerField(default=1)
+    exp = models.IntegerField(default=1)
+
+    def __str__(self):
+        return f"{self.nombre} - {self.nivel}"
+
+
+class Personaje(models.Model):
+    '''Carreras de cada sede'''
+    nombre = models.CharField(max_length=300)
+    nivel = models.IntegerField(default=1)
+
+    def __str__(self):
+        return f"{self.nombre} - {self.nivel}"
